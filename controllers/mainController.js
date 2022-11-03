4ce7b491caecf903d677ebb373ffac4880d5d4ea
AlejandroUcan <alejandro.ucan.uady@gmail.com>
2022-11-02 21:22:30 -0600

    Initial commit

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,16 @@
 const controller = {
-	index: (req, res) => {
-		return res.render('index');
-	}
+  index: (req, res) => {
+	return res.render('index');
+  },
+  main: (req, res) => {
+	return res.render('main');
+  },
+  about: (req, res) => {
+	return res.render('about');
+  },
+  contact: (req, res) => {
+	return res.render('contact');
+  }
 }
 
 module.exports = controller;
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+//Controller
 const mainController = require('../controllers/mainController');
 
+//Middleware
+const guestMiddleware = require('../middlewares/guestMiddleware');
+
+// Inicio
 router.get('/', mainController.index);
+
+// Inicio
+router.get('/main', guestMiddleware, mainController.main);
+
+// Acerca de
+router.get('/about', guestMiddleware, mainController.about);
+
+// Contacto
+router.get('/contact', guestMiddleware, mainController.contact);
 
 module.exports = router;
